d295fda2b8662f3606bd03d1b31ad6596b967ac2
ALYSSON Vicuna de Oliveira <alysson.oliveira@poupex.com.br>
2019-03-21 16:53:06 -0300

    [PRIMEIRA REVISAO]

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title>Basic Upload</title>
+</head>
+<body>
+<form action="#" method="POST" enctype="multipart/form-data">
+    <input type="file" name="fileUpload">
+    <input type="submit" value="Enviar">
+</form>
+</body>
+</html>
+
+<?php
+echo "</h1> PAGINA PARA EXIBIR OS RESULTADOS DOS TESTES </h1>"
+?>
